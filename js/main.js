f2b394ab85b87abc3ccf35efb94e17d877623166
leschb <kurultae@yandex.ru>
2024-09-27 18:52:37 +0300

    Sections testimonials and cta-calculation

--- js/main.js
+++ js/main.js
@@ -9,3 +9,41 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(document.querySelector('.cta-cooperation'));
+observer.observe(document.querySelector('.cta-calculation'));
+
+// slider
+
+const slider = document.querySelector('.slider');
+const testimonials = document.querySelector('.testimonials__list');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const sliderElement = document.querySelector('.testimonials__item');
+
+if (slider) {
+    next.addEventListener('click', () => {
+        var width = 356;
+        var gap = 24;
+        var current = getComputedStyle(slider).marginLeft;
+
+        const minMargin = -(parseInt(width) + parseInt(gap)) + 'px';
+
+        slider.style.marginLeft = parseInt(current) - parseInt(gap) - parseInt(width) + 'px';
+
+        if (slider.style.marginLeft < minMargin) {
+            slider.style.marginLeft = '0px';
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        var width = 356;
+        var gap = 24;
+        var current = getComputedStyle(slider).marginLeft;
+        const maxMargin = 380 + 'px';
+        slider.style.marginLeft = parseInt(current) + parseInt(gap) + parseInt(width) + 'px';
+
+        if (slider.style.marginLeft >= 0 + 'px') {
+            slider.style.marginLeft = '0px';
+
+        }
+    });
+}
